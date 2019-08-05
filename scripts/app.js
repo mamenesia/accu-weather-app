bc923ac2fed7e57460c524970d8d60002f62d9d0
mamenesia <dedy.prasetyo.h@gmail.com>
2019-08-05 21:02:15 +0700

    get city data from user input in form

---
+++ scripts/app.js
@@ -0,0 +1,26 @@
+const cityForm = document.querySelector('form');
+
+const updateCity = async (city) => {
+  const cityData = await getCity(city);
+  const weather = await getWeather(cityData.Key);
+
+  return {
+    cityData: cityData,
+    weather: weather
+  }
+}
+
+cityForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // get city value 
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+
+  // update the ui with the new city data
+  updateCity(city)
+    .then(data => {
+      console.log(data);
+    })
+    .catch(err => console.log(err));
+})
