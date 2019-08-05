786a6a4ef866c3c067d869e83b1626a0d8504d68
mamenesia <dedy.prasetyo.h@gmail.com>
2019-08-05 21:30:20 +0700

    updating card UI with weather data

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,34 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+  const cityData = data.cityData;
+  const weather = data.weather;
+
+  // update details template
+  details.innerHTML = `
+        <h5 class="my-3">${cityData.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+          <span>${weather.Temperature.Metric.Value}</span>
+          <span>&deg;C</span>
+        </div>
+  `;
+
+  // remove the d-none class if present
+  if (card.classList.contains('d-none')) {
+    card.classList.remove('d-none');
+  }
+}
 
 const updateCity = async (city) => {
   const cityData = await getCity(city);
   const weather = await getWeather(cityData.Key);
 
   return {
-    cityData: cityData,
-    weather: weather
+    cityData,
+    weather
   }
 }
 
@@ -20,7 +42,7 @@ cityForm.addEventListener('submit', (e) => {
   // update the ui with the new city data
   updateCity(city)
     .then(data => {
-      console.log(data);
+      updateUI(data);
     })
     .catch(err => console.log(err));
 })
--- scripts/forecast.js
+++ scripts/forecast.js
@@ -22,12 +22,3 @@ const getWeather = async (id) => {
 
   return data[0];
 }
-
-getCity('manchester')
-  .then(data => {
-    return getWeather(data.Key);
-  })
-  .then(data => {
-    console.log(data)
-  })
-  .catch(err => console.log(err));
